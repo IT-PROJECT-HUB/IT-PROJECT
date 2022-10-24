dbe45ced2d22277f7857f854f7f7df82614dbb51
IT-PROJECT-HUB <79152897+IT-PROJECT-HUB@users.noreply.github.com>
2022-10-24 17:14:37 +0300

    Выдача ролей по нажатию на реакцию Discord Bot
    Выдача ролей по нажатию на реакцию Discord Bot

---
+++ RoleBot/config.py
@@ -0,0 +1,11 @@
+BOT_TOKEN = "API KEY"  # Your Api Key
+ID_POST = 0  # Your Post ID
+USER_ROLES_LIST = ()
+MAX_ROLES = 3
+
+ROLES_LIST = {  # Your roles ID and emoji
+    "🐍": 0,  # Python
+    "🐘": 1,  # PHP
+    "🤖": 2,  # C++
+    "💽": 3,  # Web
+}
