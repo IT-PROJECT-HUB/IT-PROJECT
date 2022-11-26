53cb1a33ae0bea84ebc9e16a60bf481ef72a5666
IT-PROJECT-HUB <79152897+IT-PROJECT-HUB@users.noreply.github.com>
2022-11-26 23:47:46 +0200

    Text-To-Speach on C++
    Text-To-Speach on C++

---
+++ Text-To-Speech/Text-To-Speech/Text-To-Speech.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <sapi.h>
+
+int main(int argc, char* argv[])
+{
+	ISpVoice *pVoice = NULL;
+
+	if (FAILED(::CoInitialize(NULL)))
+		return FALSE;
+
+	HRESULT hr = CoCreateInstance(CLSID_SpVoice, NULL, CLSCTX_ALL, IID_ISpVoice, (void **)&pVoice);
+
+	if (SUCCEEDED(hr))
+	{
+		hr = pVoice->Speak(L"Подпишись на канал Неор Блог Айти", 0, NULL);
+		pVoice->Release();
+		pVoice = NULL;
+	}
+
+	::CoUninitialize();
+	return TRUE;
+}
